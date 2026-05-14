c67f7e67f29bf2b61f5abf354f08980bd6ac1f7c
Michiel Wouters <Michiel.Wouters@thomasmore.be>
2026-05-14 22:27:05 +0200

    Add project files.

---
+++ WebApiDemo/Controllers/WelkomController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WelkomController : ControllerBase
+    {
+        // Dit endpoint reageert op: GET /welkom
+        [HttpGet]
+        public string GetWelkomBericht()
+        {
+            return "Welkom bij Programming Advanced! Je eerste eigen endpoint werkt 🎉";
+        }
+
+        // Dit endpoint reageert op: GET /welkom/gepersonaliseerd/Anna
+        [HttpGet("gepersonaliseerd/{naam}")]
+        public string GetGepersonaliseerdBericht(string naam, string bar)
+        {
+            return $"Welkom bij Programming Advanced, {naam}! 🚀";
+        }
+    }
+}
