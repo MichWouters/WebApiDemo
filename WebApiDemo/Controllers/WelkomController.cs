8cb52d75de2539ec7d81f679cca113f04c3ff077
Michiel Wouters <michiel_woute64@hotmail.com>
2026-08-15 15:40:21 +0200

    Remove erronous 'bar' parameter from controller

--- WebApiDemo/Controllers/WelkomController.cs
+++ WebApiDemo/Controllers/WelkomController.cs
@@ -16,7 +16,7 @@ namespace WebApiDemo.Controllers
 
         // Dit endpoint reageert op: GET /welkom/gepersonaliseerd/Anna
         [HttpGet("gepersonaliseerd/{naam}")]
-        public string GetGepersonaliseerdBericht(string naam, string bar)
+        public string GetGepersonaliseerdBericht(string naam)
         {
             return $"Welkom bij Programming Advanced, {naam}! 🚀";
         }
